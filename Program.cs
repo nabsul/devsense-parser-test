0bca150fd54663673d0878571ba2caf3a59b29a9
Nabeel Sulieman <nabeel.sulieman@live.com>
2017-06-20 14:15:08 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace devsense_parser_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToParse = "<?php  $x = <<<EOTXT\n  \"closure\" => Closure {{$r}\n  }";
+            Lexer lexer = new Lexer(new StringReader(stringToParse), Encoding.UTF8, null, LanguageFeatures.Php71Set);
+            Tokens token;
+            while ((token = lexer.GetNextToken()) != Tokens.EOF)
+            {
+                Console.WriteLine($"Type: {token.ToString()}");
+            }        }
+    }
+}
