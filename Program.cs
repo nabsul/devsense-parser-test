9cbeca6d72d2aabb0e81f65a46f6bbff7a9b3706
Nabeel Sulieman <nabeel.sulieman@live.com>
2017-06-21 08:01:17 -0700

    Add file for testing

--- Program.cs
+++ Program.cs
@@ -9,6 +9,22 @@ namespace devsense_parser_test
     {
         static void Main(string[] args)
         {
+            TestFile();
+        }
+
+        static void TestFile()
+        {
+            var fileName = "CliDumperTest.php";
+            Lexer lexer = new Lexer(new StreamReader(File.OpenRead(fileName)), Encoding.UTF8, null, LanguageFeatures.Php71Set);
+            Tokens token;
+            while ((token = lexer.GetNextToken()) != Tokens.EOF)
+            {
+                Console.WriteLine($"Type: {token.ToString()}");
+            }                    
+        }
+
+        static void TestString() 
+        {
             string stringToParse = "<?php  $x = <<<EOTXT\n  \"closure\" => Closure {{$r}\n  }";
             Lexer lexer = new Lexer(new StringReader(stringToParse), Encoding.UTF8, null, LanguageFeatures.Php71Set);
             Tokens token;
